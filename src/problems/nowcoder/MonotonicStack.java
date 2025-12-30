69c22c86a8f60498ad1d0a4aa0dac5381ca324ee
ouyangliuy <745252780@qq.com>
2025-12-30 22:58:55 +0800

    monotonic stack

---
+++ src/problems/nowcoder/MonotonicStack.java
@@ -0,0 +1,64 @@
+package problems.nowcoder;
+
+import java.io.*;
+
+public class MonotonicStack {
+
+    public static int MAXN = 1000001;
+
+    public static int[] arr = new int[MAXN];
+
+    public static int[] stack = new int[MAXN];
+//    public static Stack stack = new Stack();
+
+    public static int[][] ans = new int[MAXN][2];
+
+    public static int n, r;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while (in.nextToken() != StreamTokenizer.TT_EOF) {
+            n = (int) in.nval;
+            for (int i = 0; i < n; i++) {
+                in.nextToken();
+                arr[i] = (int) in.nval;
+            }
+            compute();
+            for (int i = 0; i < n; i++) {
+                String res = ans[i][0] + " " + ans[i][1];
+                out.println(res);
+            }
+        }
+    }
+
+    private static void compute() {
+        int r = 0;
+        int cur;
+        for (int i = 0; i < n; i++) {
+            while (r > 0 && arr[stack[r - 1]] >= arr[i]) {
+                cur = stack[--r];
+                //
+                ans[cur][0] = r > 0 ? stack[r - 1] : -1;
+                ans[cur][1] = i;
+            }
+            stack[r++] = i;
+        }
+        // 栈中仍然有数据
+        while (r > 0) {
+            cur = stack[--r];
+            ans[cur][0] = r > 0 ? stack[r - 1] : -1;
+            ans[cur][1] = -1;
+        }
+        //如果存在相同的数据，那么需要更新这个数据
+        // 跳过n-1位置，因为它一定是正确值
+        // 只更新右侧的值，因为左侧一定是正确的
+        for (int i = n - 2; i >= 0; i--) {
+            if (ans[i][1] != -1 && arr[ans[i][1]] == arr[i]) {
+                ans[i][1] = ans[ans[i][1]][1];
+            }
+        }
+
+    }
+}
