4d517507e1f3ace400bdf1efafa2e8849ca3ea17
yuilgnayuo@gmail.com <yuilgnayuo@gmail.com>
2026-01-10 17:23:48 +0800

    tree node BFS opt Z sys out

--- src/algo/zuo/code36/Code02_ZigzagLevelOrderTraversal.java
+++ src/algo/zuo/code36/Code02_ZigzagLevelOrderTraversal.java
@@ -1,11 +1,12 @@
 package algo.zuo.code36;
 
-import java.util.*;
 
-// 二叉树的层序遍历
-// 测试链接 : https://leetcode.cn/problems/binary-tree-level-order-traversal/
-public class Code01_LevelOrderTraversal {
+import java.util.ArrayList;
+import java.util.List;
 
+// 二叉树的锯齿形层序遍历
+// 测试链接 : https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/
+public class Code02_ZigzagLevelOrderTraversal {
     public class TreeNode {
         int val;
         TreeNode left;
@@ -25,35 +26,6 @@ public class Code01_LevelOrderTraversal {
         }
     }
 
-    // 不是最优解
-    public List<List<Integer>> levelOrder1(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root != null) {
-            Queue<TreeNode> queue = new LinkedList<>();
-            HashMap<TreeNode, Integer> levs = new HashMap<>();
-            queue.add(root);
-            levs.put(root, 0);
-            while (!queue.isEmpty()) {
-                TreeNode cur = queue.poll();
-                int lev = levs.get(cur);
-                if (ans.size() == lev) {
-                    // init lev ans
-                    ans.add(new ArrayList<>());
-                }
-                ans.get(lev).add(cur.val);
-                if (cur.left != null) {
-                    queue.add(cur.left);
-                    levs.put(cur.left, lev + 1);
-                }
-                if (cur.right != null) {
-                    queue.add(cur.right);
-                    levs.put(cur.right, lev + 1);
-                }
-            }
-        }
-        return ans;
-    }
-
     // 如果测试数据量变大了就修改这个值
     public static int MAXN = 2001;
     // 使用数组代替队列结构
@@ -61,8 +33,9 @@ public class Code01_LevelOrderTraversal {
 
     public static int l, r;
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
+        boolean reverse = false;
         if (root != null) {
             l = r = 0;
             queue[r++] = root;
@@ -70,9 +43,20 @@ public class Code01_LevelOrderTraversal {
                 // 一次遍历一行的数据
                 int size = r - l; // 遍历多长，提前计算好，因为每次遍历加入数据的时候都会更改r和l
                 List<Integer> levAns = new ArrayList<>();
-                for (int i = l; i < r ; i++) {
-                    levAns.add(queue[i].val);
+                // 先收集答案，一次收集一正行的数据
+                // reverse = false ：l    -》 r-l
+                // reverse = true  : r-l  -》 l
+                if (!reverse) {
+                    for (int i = l; i < r; i++) {
+                        levAns.add(queue[i].val);
+                    }
+                } else {
+                    for (int i = r - 1; i >= l; i--) {
+                        levAns.add(queue[i].val);
+                    }
                 }
+
+                // 然后宽度遍历
                 for (int i = 0; i < size; i++) {
                     TreeNode cur = queue[l++];
                     if (cur.left != null) {
@@ -82,6 +66,7 @@ public class Code01_LevelOrderTraversal {
                         queue[r++] = cur.right;
                     }
                 }
+                reverse = !reverse;
                 ans.add(levAns);
 
             }
